987bacc0d41a6c447eebba28127da3924311d6b1
xenu-xore <52954685+xenu-xore@users.noreply.github.com>
2020-11-06 17:35:39 +0300

    play stanza and pytorch

--- textlib/Clear.py
+++ textlib/Clear.py
@@ -1,16 +1,19 @@
+# import stanza
+# from spacy_stanza import StanzaLanguage
+
 class ClearText:
     def __init__(self, corpus):
         self.corpus = corpus
 
     def corpus_text(self):
         trans_table = {ord(c): None for c in u'\r\t'}
-        dela = ' '.join(self.corpus).replace('/', '').replace(',', '').replace(';', '').replace(':', '')\
-            .replace('/', '').replace("'", '').replace('_', '').replace('-', '').replace('}', '').replace('{', '')\
-            .replace(')', '').replace('(', '').replace('?', '').replace('=', '').replace('&', '').replace('#', '')\
-            .replace('+', '').replace('.', '').replace(']', '').replace('[', '').replace("\'", '').replace("$", '')\
-            .replace('"', '').replace('*', '').replace('«', '').replace('»', '').replace('»', '').replace('>','')\
-            .replace('<','').replace('%','').replace('|','').replace('!','').replace('\\','').replace('@','')\
-            .replace('~','')
+        dela = ' '.join(self.corpus).replace('/', '').replace(',', '').replace(';', '').replace(':', '') \
+            .replace('/', '').replace("'", '').replace('_', '').replace('-', '').replace('}', '').replace('{', '') \
+            .replace(')', '').replace('(', '').replace('?', '').replace('=', '').replace('&', '').replace('#', '') \
+            .replace('+', '').replace('.', '').replace(']', '').replace('[', '').replace("\'", '').replace("$", '') \
+            .replace('"', '').replace('*', '').replace('«', '').replace('»', '').replace('»', '').replace('>', '') \
+            .replace('<', '').replace('%', '').replace('|', '').replace('!', '').replace('\\', '').replace('@', '') \
+            .replace('~', '')
 
         res_pos = [i for i, e in enumerate(dela + 'Z') if e.isupper()]
         res_list = [dela[res_pos[j]:res_pos[j + 1]] for j in range(len(res_pos) - 1)]
@@ -19,5 +22,5 @@ class ClearText:
         return corpus_text_reform
 
 
-def Clear():
-    return None
+class lematization(object):
+    pass
